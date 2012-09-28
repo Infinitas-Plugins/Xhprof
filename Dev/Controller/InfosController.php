dc155a9734eb9e0af98458fda08eac8929086e00
dogmatic69 <dogmatic69@gmail.com>
2012-09-28 13:50:15 +0100

    white space

--- Dev/Controller/InfosController.php
+++ Dev/Controller/InfosController.php
@@ -1,11 +1,11 @@
 <?php
-	/* 
+	/*
 	 * Short Description / title.
-	 * 
+	 *
 	 * Overview of what the file does. About a paragraph or two
-	 * 
+	 *
 	 * Copyright (c) 2010 Carl Sutton ( dogmatic69 )
-	 * 
+	 *
 	 * @filesource
 	 * @copyright Copyright (c) 2010 Carl Sutton ( dogmatic69 )
 	 * @link http://www.infinitas-cms.org
@@ -13,24 +13,24 @@
 	 * @subpackage {see_below}
 	 * @license http://www.opensource.org/licenses/mit-license.php The MIT License
 	 * @since {check_current_milestone_in_lighthouse}
-	 * 
+	 *
 	 * @author {your_name}
-	 * 
+	 *
 	 * Licensed under The MIT License
 	 * Redistributions of files must retain the above copyright notice.
 	 */
 
 	class InfosController extends DevAppController {
 		public $uses = false;
-		
+
 		public function __construct($request = null, $response = null) {
 			parent::__construct($request, $response);
-			
+
 			unset($this->components['Filter.Filter']);
 		}
-		
+
 		public function admin_index() {
-			
+
 		}
 
 		public function admin_symlink() {
